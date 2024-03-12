a5cb97a43a2003b448feb467e3c68b11c8e9e03a
jackBlaster <thibault.biville@gmail.com>
2024-03-12 12:42:58 +0100

    first commit

--- src/nodes/node.ts
+++ src/nodes/node.ts
@@ -1,7 +1,10 @@
 import bodyParser from "body-parser";
 import express from "express";
 import { BASE_NODE_PORT } from "../config";
-import { Value } from "../types";
+import { Value,NodeState } from "../types";
+import { startConsensus, stopConsensus } from "./consensus";
+
+
 
 export async function node(
   nodeId: number, // the ID of the node
@@ -18,23 +21,85 @@ export async function node(
 
   // TODO implement this
   // this route allows retrieving the current status of the node
-  // node.get("/status", (req, res) => {});
+  node.get("/status", (req, res) => {
+    if(isFaulty) {
+      res.status(500).send("faulty")
+    }
+    else{
+      res.status(200).send("live")
+    }
+  });
 
   // TODO implement this
   // this route allows the node to receive messages from other nodes
-  // node.post("/message", (req, res) => {});
+  node.post("/message", (req, res) => {
+    
+    res.status(200).send("Message received");
+  });
 
   // TODO implement this
   // this route is used to start the consensus algorithm
-  // node.get("/start", async (req, res) => {});
+  node.get("/start", async (req, res) => {
+    let ready = nodesAreReady() && !isFaulty;
+
+    while (!ready) {
+      for (let i = 0; i < N; i++) {
+        setNodeIsReady(i);
+      }
+      ready = nodesAreReady() && !isFaulty;
+    }
+    for (let i =0; i<N;i++){
+      const nodeUrl = `http://localhost:${BASE_NODE_PORT + i}/message`;
+      const message = "Hello, nodes! Consensus is starting.";
+
+      try {
+        await fetch(nodeUrl, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ message }),
+        });
+
+        console.log(`Message sent to Node ${i}: ${message}`);
+      } catch (error:any) {
+        console.error(`Failed to send message to Node ${i}: ${error.message}`);
+      }
+    }
+  
+    res.status(200).send("Consensus algorithm started.");
+
+  });
 
   // TODO implement this
   // this route is used to stop the consensus algorithm
-  // node.get("/stop", async (req, res) => {});
+  node.get("/stop", async (req, res) => {
+    
+    res.status(200).send("Stopped")
+  });
 
   // TODO implement this
   // get the current state of a node
-  // node.get("/getState", (req, res) => {});
+  node.get("/getState", (req, res) => {
+    let state : NodeState;
+    if(isFaulty) {
+      state = {
+        killed: false,
+        x:initialValue,
+        decided:null,
+        k:null
+      }
+    }
+    else {
+        state = {
+          killed:false,
+          x:initialValue,
+          decided:false,
+          k:0
+        }
+    }
+    res.status(200).send(state)
+  });
 
   // start the server
   const server = node.listen(BASE_NODE_PORT + nodeId, async () => {
@@ -48,3 +113,5 @@ export async function node(
 
   return server;
 }
+
+
